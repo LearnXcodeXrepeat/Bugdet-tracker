ee338014d1965676281e0926f582a5402b607203
viraljain7 <veerljain1234@gmail.com>
2024-06-16 13:41:16 +0530

    Add functionality to AddExpense

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,20 +1,47 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addExpense } from '../context/BudgetAndExpenseSlice';
 import './AddExpenseForm.css';
 
 function AddExpenseForm() {
-    const [productName, setProductName] = useState("")
-    const [amount, setAmount] = useState("")
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const amountNumber = parseFloat(amount);
+        if (!isNaN(amountNumber)) {
+            const newExpense = { id: Date.now(), name, amount: amountNumber };
+            dispatch(addExpense(newExpense));
+            setName('');
+            setAmount('');
+        } else {
+            console.error('Invalid amount entered');
+        }
+    };
+
     return (
         <div className="add-expense-form">
             <h2>Add New Expense</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label>Expense Name</label>
-                    <input type="text" placeholder="Enter expense name" value={productName} onChange={(e) => setProductName(e.target.value)} />
+                    <input
+                        type="text"
+                        placeholder="Enter expense name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div className="form-control">
                     <label>Amount</label>
-                    <input type="text" placeholder="Enter amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                    <input
+                        type="number"
+                        placeholder="Enter amount"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                    />
                 </div>
                 <button type="submit">Add Expense</button>
             </form>
