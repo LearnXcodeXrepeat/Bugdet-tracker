bdc8d5e5e58158b0cef5ce79d395f99fa47f6c12
viraljain7 <veerljain1234@gmail.com>
2024-06-16 13:40:28 +0530

    un used file remove action and reducer

--- src/context/actions.js
+++
@@ -1,9 +0,0 @@
-export const setBudget = (budget) => ({
-    type: 'SET_BUDGET',
-    payload: budget,
-});
-
-export const addExpense = (expense) => ({
-    type: 'ADD_EXPENSE',
-    payload: expense,
-});
