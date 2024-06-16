137925357a3b3f894f7d99e9206b41cf10e28ff3
viraljain7 <veerljain1234@gmail.com>
2024-06-16 13:42:00 +0530

    Add functionality to ExpenseList

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,22 +1,21 @@
-// import React from 'react';
+import { useSelector } from 'react-redux';
 import './ExpenseList.css';
 
-const expenses = [
-    { id: 1, name: 'Groceries', amount: 50.00 },
-    { id: 2, name: 'Rent', amount: 1200.00 },
-    { id: 3, name: 'Utilities', amount: 200.00 },
-    { id: 4, name: 'Internet', amount: 60.00 },
-];
-
 function ExpenseList() {
+    const { expenses } = useSelector(state => state.budget);
+
     return (
         <div className="expense-list">
-            <h2>Expenses</h2>
+            <h2>Expense List</h2>
             <ul>
-                {expenses.map(expense => (
-                    <li key={expense.id}>
-                        <span>{expense.name}</span>
-                        <span className="expense-amount">${expense.amount.toFixed(2)}</span>
+                {expenses.map((expense, index) => (
+                    <li className="box" key={index}>
+                        <div className='left'  >
+                            {expense.name}
+                        </div>
+                        <div className="right">
+                            ${expense.amount.toFixed(2)}
+                        </div>
                     </li>
                 ))}
             </ul>
