944d43383c2f826cf7a8c329f7bdab4fd41c96db
viraljain7 <veerljain1234@gmail.com>
2024-06-16 13:42:25 +0530

    Store Added to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import BudgetOverview from './components/BudgetOverview';
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Provider } from 'react-redux';
+import store from './context/store';
+// import * as serviceWorker from './serviceWorker';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </React.StrictMode>,
 )
+
+
+
+
+
+// serviceWorker.unregister();
